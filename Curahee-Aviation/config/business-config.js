a62d111ca69b2e4214084af6404c3e01d4a0b90e
Michael Pabst <mikeypbr3@gmail.com>
2025-09-01 17:17:50 -0400

    Modified dry rate

--- Curahee-Aviation/config/business-config.js
+++ Curahee-Aviation/config/business-config.js
@@ -1,7 +1,7 @@
 const BUSINESS_CONFIG = {
     tagline: "Student pilots welcome", 
     wetRate: "$180",
-    dryRate: "Available on req.", 
+    dryRate: "Dry rate on request", 
     aircraftYear: "1965",
     aircraftModel: "Cessna 172"
 };
